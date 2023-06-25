2b03abf0b5fe219fdba36666bd377e4f0567eaab
Plateurs <josehlpv@gmail.com>
2023-06-25 15:10:11 -0400

    agregados las configs a los archivos .js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge:[
+        './public/**/*.html'
+    ],
+    darkmode: false, // or 'media' or 'class'
+    theme:{
+        extend:{}
+    },
+    variants:{
+        extend:{}
+    },
+    plugins:[],
+
+}
